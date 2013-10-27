2a65ac7f7339eb49b263aae7fa071993cdf129a2
Tanawat Tassana <amp.tanawat@gmail.com>
2013-10-28 05:38:34 +0700

    convert js to angularjs

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-(function() {
-    var app = new Firebase('https://factorygame.firebaseio.com');
-    var auth = new FirebaseSimpleLogin(app, function(error, user) {
-        if(error) {
-            console.error('An error occurred while attempting login', error);
-        } else if(user) {
-            console.info('User ID: ' + user.id + ', Provider: ' + user.provider);
-        } else {
-            console.info('No user in this client.');
-        }
-    });
-})();
+var FactoryGame = angular.module('FactoryGame', []);
+
+FactoryGame.factory('FirebaseApp', [
+    'constants.Firebase', 'constants.FirebaseAppUrl', 
+    function(Firebase, appUrl) {
+        return new Firebase(appUrl);
+    }
+]);
+
+FactoryGame.factory('FirebaseAuth', [
+    'constants.FirebaseLogin', 'FirebaseApp',
+    function(FirebaseLogin, app) {
+        return new FirebaseLogin(app, function(error, user) {
+            if(error) {
+                console.error('An error occurred while attempting login', error);
+            } else if(user) {
+                console.info('User ID: ' + user.id + ', Provider: ' + user.provider);
+            } else {
+                console.info('No user in this client.');
+            }
+        });
+    }
+]); 
+
