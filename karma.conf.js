7b9ed7581d3a091028ed09ab5e2bbac914e13dea
Tanawat Tassana <amp.tanawat@gmail.com>
2013-10-28 07:15:50 +0700

    add underscore

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,10 @@ module.exports = function(config) {
         autoWatch: true,
         browsers: ['Chrome'],
         files: [
+            'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
             'https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js',
             'lib/angular-mocks.js',
+            'lib/sinon-1.7.3.js',
             'app.js',
             'tests.js'
         ],
